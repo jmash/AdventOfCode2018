281d551efe87531f8cc640d3c268010744197ad2
jmash <jaredmashcraft@gmail.com>
2019-02-02 23:44:52 -0500

    :recycle: improves and completes part 2 of the challenge

--- day1/day1.js
+++ day1/day1.js
@@ -5,22 +5,48 @@ let rl = readline.createInterface(
     fs.createReadStream('./day1input.txt', 'utf-8')
 );
 
-let currFreq = 0;
-let seenFreqs = [0];
+const part1Flag = false; // switch to false for part 2
+
+let freqChanges = [];
 
 rl.on('line', (line) => {
-    if(line[0] === '+') {
-        currFreq += parseInt(line.substring(1));
-    } else {
-        currFreq -= parseInt(line.substring(1));
-    }
-    if(checkDups(seenFreqs, currFreq)) 
-    seenFreqs.push(currFreq);
+    freqChanges.push(parseInt(line));    
+}).on('close', () => {
+    if(part1Flag) addList();
+    else findDupFreq();
 });
 
-function checkDups(a, p) {
-    for(let i = 0; i < a.length; i++) {
-        if (a[i] === p) {
+function addList() {
+    console.log(freqChanges.reduce((a, b) => a + b));
+}
+
+function findDupFreq() {
+    // initialize list for resultant frequencies
+    resFreqs = [];
+    // intialize current frequency
+    currFreq = 0;
+    // start loop to iterate through frequency changes
+    for(let i = 0; i < freqChanges.length; i++) {
+        // add current frequency to resultant frequency list
+        resFreqs.push(currFreq);
+        // apply frequency change to current frequency
+        currFreq += freqChanges[i];
+        // check to see if the updated frequency matches a resultant frequency already listed
+        if(checkDups(resFreqs, currFreq)) {
+            // if so, log the updated frequency and break the loop
+            console.log(currFreq);
+            break;
+        }
+        // if the list ends before finding the duplicate frequency, start loop over
+        if(i === freqChanges.length - 1) {
+            i = -1;
+        }
+    }
+}
+
+function checkDups(arr, j) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === j) {
             return true;
         }
     }
